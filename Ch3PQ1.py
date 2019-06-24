dd4989ae198480db310e085a7b9cff0c01a338ae
Steve Ayers <steveayers124@gmail.com>
2019-06-24 17:29:00 -0500

    Ch3PQ1.py Collatz Sequence

---
+++ Ch3PQ1.py
@@ -0,0 +1,30 @@
+# given an input value to begin, print its Collatz Sequence
+
+def collatz(number):
+    remainder = number % 2
+    if remainder == 0:
+        try:
+            result = number // 2
+        except ZeroDivisionError:
+            print('Error: diviision by zero')
+    elif remainder == 1:
+        result = (number * 3) + 1
+    return result
+
+print('\n')
+print('=========================================================================================')
+print('Welcome to the Collatz Sequence Generator, the World\'s Simplist Impossible Math Problem!')
+print('You enter a number, and I\'ll give the Collatz Sequence for it. This always ends in 1.')
+seed = 0
+while not (seed > 0):
+    print('Please enter a number greater than 0.')
+    seed = int(input())
+print('- ' + str(seed) + ' - ', sep='', end='')
+# if seed == 1:
+#     exit()
+value = seed
+while value != 1:
+    value = collatz(value)
+    print(str(value) + ' - ', sep='', end='')
+print('\n=========================================================================================')
+print('\n')
