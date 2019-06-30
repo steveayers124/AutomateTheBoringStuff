57954a2126c650f6419d88c81d6b1716d9f2ffff
Steve Ayers <steveayers124@gmail.com>
2019-06-30 08:19:37 -0500

    Ch4Review.py: Review AutomateTheBoringStuff Chapter 4 principles, test out a few things

---
+++ Ch4Review.py
@@ -0,0 +1,21 @@
+# Review AutomateTheBoringStuff Chapter 4 principles, test out a few things
+
+spam = ['a','b','c','d']
+bacon = [3.14, 'cat', 11, 'cat', True]
+print(len(spam))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
