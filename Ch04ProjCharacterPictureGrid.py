c5b0ffcfa7a0ff85c08edad001f86bca99ed9358
Steve Ayers <steveayers124@gmail.com>
2019-07-07 17:29:34 -0500

    feat: transpose, to print column major order

--- Ch04ProjCharacterPictureGrid.py
+++ Ch04ProjCharacterPictureGrid.py
@@ -1,10 +1,16 @@
 # Given a list of lists having one character per item,
 #  print every character in the lists in order to produce a picture.
 
-def printCharacterGrid(grid):
+def printStringGrid(grid):
     for i in grid:
         for j in i:
             print(str(j)[0], end='')
+        print('')
+
+def printCharacterGrid(grid):
+    for i in range(len(grid[0])):
+        for j in range(len(grid)):
+            print(str(grid[j][i])[0], end='')
         print('')
 
 grid = [['.', '.', '.', '.', '.', '.'],
