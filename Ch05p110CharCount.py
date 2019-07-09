9ad9829bc87b922f34dfdb265b58e22dd46d1ee8
Steve Ayers <steveayers124@gmail.com>
2019-07-09 12:29:43 -0500

    Ch05p110CharCount.py: practical use of .setdefault() method, and use of pretty print module

---
+++ Ch05p110CharCount.py
@@ -0,0 +1,13 @@
+# Use the dictionary datatype's .setdefault() method to aid in counting incidence of
+# symbols in a string.
+
+import pprint
+
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+
+count = {}
+for symbol in message:
+    count.setdefault(symbol, 0)
+    count[symbol] += 1
+
+pprint.pprint(count)
