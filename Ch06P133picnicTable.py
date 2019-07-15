47bfe84a805177460df4f7cfed2e8c29359e4144
Steve Ayers <steveayers124@gmail.com>
2019-07-15 09:15:54 -0500

    feat: make this printPicnic() function more general, able to print dictionaries whose keys are numeric

--- Ch06P133picnicTable.py
+++ Ch06P133picnicTable.py
@@ -1,7 +1,7 @@
 def printPicnic(itemsDict, leftWidth, rightWidth):
     print('PICNIC ITEMS'.center(leftWidth + rightWidth, '-'))
     for k, v in itemsDict.items():
-        print(k.ljust(leftWidth, '.') + str(v).rjust(rightWidth))
+        print(str(k).ljust(leftWidth, '.') + str(v).rjust(rightWidth))
 
 
 picnicItems = {'sandwiches': 4, 'apples': 12, 'cups': 4, 'cookies': 8000}
