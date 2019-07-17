70b24d8f46adb81dcc0efefc06b492577541e27c
Steve Ayers <steveayers124@gmail.com>
2019-07-17 08:47:09 -0500

    feat: copy the password

--- Ch06Proj1Password.py
+++ Ch06Proj1Password.py
@@ -5,7 +5,7 @@ PASSWORDS = {'email': 'laskdy091832i4erfakljsldiuvs4uyot2358irp0-guw9vjhr8bn ',
              'blog': 'z.x,mcnvaksdjhfpvo9384hglku34yfhlakenr',
              'luggage': '12345'}
 
-import sys
+import sys, pyperclip
 if len(sys.argv) < 2:
     print('Usage: python3 Ch06Proj1Password.py [account] - copy account password')
     sys.exit()
@@ -14,3 +14,9 @@ account = sys.argv[1]    # first command line arg is the account name
 print(sys.version)
 print(sys.version_info)
 print(sys.api_version)
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print('Password for ' + account + ' copied to clipboard.')
+else:
+    print('There is no account named ' + account)
