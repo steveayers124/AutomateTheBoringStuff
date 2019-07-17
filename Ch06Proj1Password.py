11a6ab0c9fc4e40d67c14b574c75e53a2f7531e5
Steve Ayers <steveayers124@gmail.com>
2019-07-17 01:09:59 -0500

    Ch06Proj1Password.py - step 1: program design and data structures

---
+++ Ch06Proj1Password.py
@@ -0,0 +1,7 @@
+#! python3
+# pw.py - An insecure password locker program.
+
+PASSWORDS = {'email': 'laskdy091832i4erfakljsldiuvs4uyot2358irp0-guw9vjhr8bn ',
+             'blog': 'z.x,mcnvaksdjhfpvo9384hglku34yfhlakenr',
+             'luggage': '12345'}
+
