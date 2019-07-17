852f3221500803583218cdb2fb2af7a2310f798c
Steve Ayers <steveayers124@gmail.com>
2019-07-17 01:18:29 -0500

    feat: step 2: Handle Command Line Arguments

--- Ch06Proj1Password.py
+++ Ch06Proj1Password.py
@@ -5,3 +5,12 @@ PASSWORDS = {'email': 'laskdy091832i4erfakljsldiuvs4uyot2358irp0-guw9vjhr8bn ',
              'blog': 'z.x,mcnvaksdjhfpvo9384hglku34yfhlakenr',
              'luggage': '12345'}
 
+import sys
+if len(sys.argv) < 2:
+    print('Usage: python3 Ch06Proj1Password.py [account] - copy account password')
+    sys.exit()
+
+account = sys.argv[1]    # first command line arg is the account name
+print(sys.version)
+print(sys.version_info)
+print(sys.api_version)
