d47a0a46c0854148d54e1da6ecb9e2f198114df8
Steve Ayers <steveayers124@gmail.com>
2019-07-20 08:03:25 -0500

    accumulate: tests and learning

--- Ch07p148isPhoneNumber2.py
+++ Ch07p148isPhoneNumber2.py
@@ -15,4 +15,28 @@ if matchObj is not None:
 line1 = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my offfice.'
 matchObj = phoneNumRegex.search(line1)
 print('Searching:|'+line1+'|\n'+'Phone number found:|'+matchObj.group()+'|')
-print('Done')
+print('Done.' + 'Now define group parts, and access parts, or whole.')
+
+r = re.compile(r'(\d{3})-(\d{3}-\d{4})')
+line1 = 'My number is 415-555-4242.'
+m = r.search(line1)
+if m is not None:
+    print('Searching |' + line1 + '| for pattern |' + str(r.pattern) + '|')
+    print(m.group(1))
+    print(m.group(2))
+    print(m.group(0))
+    print(m.group())
+print('Done.' + 'Now multiple-assign that tuple.')
+if m is not None:
+    print(m.groups())
+    areaCode, mainNumber = m.groups()
+    print('The areacode was ' + areaCode + ', and the main number was ' + mainNumber + '.')
+print('Done.' + 'Now how do you deal with real parens?')
+
+p = re.compile(r'(\(\d{3}\)) (\d{3}-\d{4})')
+line1 = 'My number is (415) 555-4242.'
+m = p.search(line1)
+if m is not None:
+    print(m.group(1))
+    print(m.group(2))
+print('Done.')
