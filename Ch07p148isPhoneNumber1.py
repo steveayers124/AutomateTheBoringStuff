3ceb012bb319e3fc03a7c218e22507439c1e32db
Steve Ayers <steveayers124@gmail.com>
2019-07-20 00:40:22 -0500

    Ch07p148isPhoneNumber1.py

---
+++ Ch07p148isPhoneNumber1.py
@@ -0,0 +1,24 @@
+# Without the use of regex, determine whether a passed string is a phone number.
+
+def isPhoneNumber(text):
+    if len(text) != 12:
+        return False
+    for i in range(0, 3):
+        if not text[i].isdecimal():
+            return False
+    if text[3] != '-':
+        return False
+    for i in range(4, 7):
+        if not text[i].isdecimal():
+            return False
+    if text[7] != '-':
+        return False
+    for i in range(8, 11):
+        if not text[i].isdecimal():
+            return False
+    return True
+
+print('415-555-4242 is a phone number:')
+print(isPhoneNumber('415-555-4242'))
+print('Moshi moshi is a phone number:')
+print(isPhoneNumber('Moshi moshi'))
