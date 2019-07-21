d32424c610d9cab9746345b01f963b984d0cd1b8
Steve Ayers <steveayers124@gmail.com>
2019-07-20 23:04:10 -0500

    feat: play with re.I flag

--- Ch07p148isPhoneNumber2.py
+++ Ch07p148isPhoneNumber2.py
@@ -15,7 +15,7 @@ if matchObj is not None:
 line1 = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my offfice.'
 matchObj = phoneNumRegex.search(line1)
 print('Searching:|'+line1+'|\n'+'Phone number found:|'+matchObj.group()+'|')
-print('Done.' + 'Now define group parts, and access parts, or whole.')
+print('Done. ' + 'Now define group parts, and access parts, or whole.')
 
 r = re.compile(r'(\d{3})-(\d{3}-\d{4})')
 line1 = 'My number is 415-555-4242.'
@@ -26,12 +26,12 @@ if m is not None:
     print(m.group(2))
     print(m.group(0))
     print(m.group())
-print('Done.' + 'Now multiple-assign that tuple.')
+print('Done. ' + 'Now multiple-assign that tuple.')
 if m is not None:
     print(m.groups())
     areaCode, mainNumber = m.groups()
     print('The areacode was ' + areaCode + ', and the main number was ' + mainNumber + '.')
-print('Done.' + 'Now how do you deal with real parens?')
+print('Done. ' + 'Now how do you deal with real parens?')
 
 p = re.compile(r'(\(\d{3}\)) (\d{3}-\d{4})')
 line1 = 'My number is (415) 555-4242.'
@@ -39,4 +39,33 @@ m = p.search(line1)
 if m is not None:
     print(m.group(1))
     print(m.group(2))
-print('Done.')
+print('Done. ' + 'Now for ignoring case sensitivity...')
+
+robocop = [re.compile('RoboCop'),
+           re.compile('ROBOCOP'),
+           re.compile('RobOCop'),
+           re.compile('RobocOp'), ]
+line = ['RoboCop is part man, part machine, all cop.',
+        'ROBOCOP protects the innocent.',
+        'Al, why does your programming book talk about robocop so much?']
+for c in range(len(robocop)):
+    for l in range(len(line)):
+        found = robocop[c].search(line[l])
+        if found is not None:
+            print(found.group() + ', pattern |' + robocop[c].pattern + '|, line |' + line[l] + '|')
+print('Done. ' + 'Okay, that ^ was all case sensitive. Default behavior.')
+print('What follows is case insensitive, using the re.IGNORECASE flag, or re.I for short.')
+
+robocop = [re.compile('RoboCop', re.IGNORECASE),
+           re.compile('ROBOCOP', re.I),
+           re.compile('RobOCop', re.I),
+           re.compile('RobocOp', re.I), ]
+line = ['RoboCop is part man, part machine, all cop.',
+        'ROBOCOP protects the innocent.',
+        'Al, why does your programming book talk about robocop so much?']
+for c in range(len(robocop)):
+    for l in range(len(line)):
+        found = robocop[c].search(line[l])
+        if found is not None:
+            print(found.group() + ', pattern |' + robocop[c].pattern + '|, line |' + line[l] + '|')
+print('Done. Now, let\'s substitute strings for the found pattern.')
