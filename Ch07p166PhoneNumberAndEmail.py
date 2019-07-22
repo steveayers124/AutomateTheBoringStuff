1b8294599cbb60bd57431fcf9f620818d5ae5069
Steve Ayers <steveayers124@gmail.com>
2019-07-22 12:06:04 -0500

    bug: email regex not working yet

--- Ch07p166PhoneNumberAndEmail.py
+++ Ch07p166PhoneNumberAndEmail.py
@@ -1,7 +1,7 @@
 # Ch07p166PhoneNumberAndEmail.py: find all phone numbers and email addresses in text copied to the system clipboard,
 # and replace what's on the clipboard with just the possible phone and email items.
 
-import pyperclip
+import pyperclip, re
 
 phoneRegex = re.compile(r'''(
     (\d{3}|\(\d{3}\))?             # area code
@@ -11,14 +11,18 @@ phoneRegex = re.compile(r'''(
     \d{4}                          # last 4 digits
     (\s*(ext|x|ext\.)\s*\d{2,5})?  # extension
     )''', re.VERBOSE | re.I)
-# TODO: compile an email regex
+
 emailRegex = re.compile(r'''
     (\w*)                          # email name
     @                              # at symbol
     (\w*)                          # company
-    .                              # dot symbol
-    (com|org|gov|edu)              # extension
+    \.                             # dot symbol
+    (com|net|org|gov|edu)          # extension
     ''')
+line1 = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
+print(phoneRegex.findall(line1))
+line2 = 'Email me at adam@cox.net tomorrow. adam@home.com is my office.'
+print(emailRegex.findall(line2))
 
 # TODO: get text to search from clipboard
 
