827ff390ff784f36f10b25187401e756625e8358
Steve Ayers <steveayers124@gmail.com>
2019-07-24 23:29:34 -0500

    fix: continue debugging- Use more () to group more elements in regex. Properly access the new set of groups.

--- Ch07p166PhoneNumberAndEmail.py
+++ Ch07p166PhoneNumberAndEmail.py
@@ -4,19 +4,19 @@
 import pyperclip, re
 
 phoneRegex = re.compile(r'''(
-    (\d{3}|\(\d{3}\))?             # area code
-    (\s|-|\.)?                     # separator
-    (\d{3})                        # first 3 digits
-    (\s|-|\.)                      # separator
-    (\d{4})                        # last 4 digits
-    (\s*(ext|x|ext\.)\s*\d{2,5})?  # extension
+    (\d{3}|\(\d{3}\))?               # area code
+    (\s|-|\.)?                       # separator
+    (\d{3})                          # first 3 digits
+    (\s|-|\.)                        # separator
+    (\d{4})                          # last 4 digits
+    (\s*(ext|x|ext\.)\s*(\d{2,5}))?  # extension
     )''', re.VERBOSE | re.I)
 
 emailRegex = re.compile(r'''(
-    [a-zA-Z0-9.\-_+%]+             # email name
-    @                              # at symbol
-    [a-zA-Z0-9.\-]+                # domain name
-    (\.[a-zA-Z]{2,4})              # dot something
+    [a-zA-Z0-9.\-_+%]+               # email name
+    @                                # at symbol
+    [a-zA-Z0-9.\-]+                  # domain name
+    (\.[a-zA-Z]{2,4})                # dot something
     )''', re.VERBOSE)
 # line1 = 'Call me at 415-555-1011 tomorrow. 415-555-9999 is my office.'
 # print(phoneRegex.findall(line1))
@@ -44,9 +44,8 @@ text = str(pyperclip.paste())
 matches = []
 for groups in phoneRegex.findall(text):
     phoneNum = '-'.join([groups[1], groups[3], groups[5]])
-    print(len(groups))
-    # if groups[8] != '':
-    #     phoneNum += ' x' + groups[8]
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
     matches.append(phoneNum)
 for groups in emailRegex.findall(text):
     matches.append(groups[0])
